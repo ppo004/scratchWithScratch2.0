85edb6b6ca39e75e508acd06837c5a4c5d4bab94
Prajwal Ponnana <prajwalshiv.04@gmail.com>
2024-07-03 19:17:13 +0530

    added manifest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,18 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');  // Add this line
 
-process.env["NODE_ENV"] = "production";
+process.env.NODE_ENV = 'production';
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
-      `...`, // Extend existing minimizers (i.e. `terser-webpack-plugin`)
+      '...', 
       new CssMinimizerPlugin(),
     ],
   },
@@ -19,6 +20,14 @@ module.exports = merge(common, {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      favicon: './public/favicon.ico' 
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: 'manifest.json' }, 
+        { from: 'public/logo192.png', to: 'logo192.png' },    
+        { from: 'public/logo512.png', to: 'logo512.png' }      
+      ]
+    })
   ],
 });
